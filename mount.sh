0d1027ac6ff7829f96ec72aba4b5a6255c6d4800
Mulliganaceous <mulliganaceous@outlook.com>
2021-09-06 01:07:26 -0400

    210906: Labor day, added a 10E301 zoom
    This upload contains an old Fractal Extreme zoom with a magnification of over 1000 zooms. It has been made by Helen Dawson and the link is https://www.cygnus-software.com/gallery/1000zooms.htm

---
+++ mount.sh
@@ -0,0 +1,6 @@
+sudo kdir /media/mulliganaceous/Mulliganaceous_S;
+sudo mount -t ntfs /dev/sda6 /media/mulliganaceous/Mulliganaceous_S;
+sudo mkdir /media/mulliganaceous/MulligOneDrive;
+sudo mount -t ntfs /dev/sda5 /media/mulliganaceous/MulligOneDrive;
+sudo mkdir /dev/sda4 /media/mulliganaceous/C1;
+sudo mount -t ntfs /dev/sda4 /media/mulliganaceous/C1
