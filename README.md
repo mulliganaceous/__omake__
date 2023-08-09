b06e7f6393b28b3be7fb1595accdd525770f7d1e
Samaria <mulliganaceous@outlook.com>
2023-08-09 18:54:00 -0400

    Update as of Civic Holiday 2023

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 It's __omake__ time!
 
 * [YouTube](https://www.youtube.com/channel/@mulliganaceous)
-* [DevPost] (https://devpost.com/Mulliganaceous)
+* [DevPost](https://devpost.com/Mulliganaceous)
+
+![Endgame Of Line 3](./Line3Endgame/Line3Endgame.PNG)
 
 御負け
--- mount.sh
+++ mount.sh
@@ -1,6 +1,17 @@
-sudo kdir /media/mulliganaceous/Mulliganaceous_S;
-sudo mount -t ntfs /dev/sda6 /media/mulliganaceous/Mulliganaceous_S;
-sudo mkdir /media/mulliganaceous/MulligOneDrive;
-sudo mount -t ntfs /dev/sda5 /media/mulliganaceous/MulligOneDrive;
-sudo mkdir /dev/sda4 /media/mulliganaceous/C1;
-sudo mount -t ntfs /dev/sda4 /media/mulliganaceous/C1
+# sudo kdir /media/mulliganaceous/Mulliganaceous_S;
+# sudo mount -t ntfs /dev/sda6 /media/mulliganaceous/Mulliganaceous_S;
+# sudo mkdir /media/mulliganaceous/MulligOneDrive;
+# sudo mount -t ntfs /dev/sda5 /media/mulliganaceous/MulligOneDrive;
+# sudo mkdir /dev/sda4 /media/mulliganaceous/C1;
+# sudo mount -t ntfs /dev/sda4 /media/mulliganaceous/C1
+
+# Mount partitions
+if ! df | grep -q /dev/nvme0n1p4; then
+  udisksctl mount --block-device=/dev/nvme0n1p4
+fi
+if ! df | grep -q /dev/nvme0n1p5; then
+  udisksctl mount --block-device=/dev/nvme0n1p5
+fi
+if ! df | grep -q /dev/nvme0n1p6; then
+  udisksctl mount --block-device=/dev/nvme0n1p6
+fi
