58bec37e467b594857ac59ae2b4cc9873310408d
Samaria Mulligan <mulliganaceous@outlook.com>
2024-06-27 04:24:24 -0400

    Added description of the Threes modules.
    Combined chart provided, explanations given for each of the three modules.

--- Threes/README.md
+++ Threes/README.md
@@ -1,9 +1,18 @@
 # Threes Analyzer
 
 This is a work in progress. All Three-related research goes there. The first project is to examine which scores are uncommon, and which scores are impossible.
-The smallest impossible score is __39363__. The list of all impossible scores is listed in impossible_scores.txt.
+
+The smallest impossible score is __39,363__. It is also the first number whose sum of ternary digits exceed 16. The list of all impossible scores is listed in impossible_scores.txt.
+
+The `card_combinations.py` module provides the recursive (converted to iterative stack) backtracking method of finding all card combinations. The `impossible_scores.py` goes through all possible scores up to a predetermined range (18,000,000) and saves a .npy file listing the score, the corresponding rank, and the number of combinations for that score. The `impossible_histogram.py` uses the .npy file to display a graph.
 
 ![Figure](combinations_by_score.gif)
+
+## All observations
+
+Plots are color coded by density and by rank, roughly analogous to the Ketchapp colors. Score is in a logarithmic scale. Tick marks are from early rank (high card only) to late rank (high card plus one of each of the lower cards) scores. Rates of impossibility are by the increments used for the line graph. Regions with low number of possible combinations per score have higher rates of impossibility at high rank.
+
+![Figure](Impossible_Percentage.png)
 
 ## External links
 
--- Threes/impossible_histogram.py
+++ Threes/impossible_histogram.py
@@ -1,28 +1,144 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import ticker
 
 card_combination_table = np.load("impossible_scores.npy")
 impossibles = card_combination_table[card_combination_table[:,1] == 0]
 rank_colors = ["blue","gray","gray","gray","#e8b280",\
                "#e9826e","#dd6044","#edda7c","#ead163", \
                "#dbbc49","#e6c541","#81d89b","#448958","black"]
-subrank_types = ["o","D","+","x"]
+subrank_types = [["o",10],["D",5],["+",21],["x",10]]
 
-fig, axs = plt.subplot_mosaic([['linear-log']], layout='constrained')
-ax = axs['linear-log']
-ax.set_yscale('linear')
-plt.xlim((-60,500))
+def possible_combinations():
+    fig, axs = plt.subplot_mosaic([['possible_combinations']], layout='constrained')
+    ax = axs['possible_combinations']
+    ax.set_yscale('linear')
 
-subtable = card_combination_table[card_combination_table[:,2] < 40]
-ax.scatter(subtable[:,1], subtable[:,0], c=(rank_colors[1],0.25))
-subtable = card_combination_table[card_combination_table[:,2] % 10 == 3]
-ax.scatter(subtable[:,1] - 50, subtable[:,0], c=('red',1/25), marker='x', s=5)
-for k in range(4,14):
-    for j in range(0,3):
+    subtable = card_combination_table[card_combination_table[:,2] < 40]
+    ax.scatter(subtable[:,1], subtable[:,0], c=(rank_colors[1],0.25), s = subrank_types[0][1])
+    plt.xlim((-5,np.max(subtable[:,1])))
+    plt.savefig("Rank{0:0d}-{1:d}".format(0,0))
+    print("Rank{0:0d}-{1:d}".format(0,0))
+
+    subtable = card_combination_table[card_combination_table[:,2] % 10 == 3]
+    ax.scatter(subtable[:,1], subtable[:,0], c=('red',1/50), marker='x', s=5)
+    for k in range(4,14):
+        for j in range(0,3):
+            subtable = card_combination_table[card_combination_table[:,2] == 10*k + j]
+            ax.scatter(subtable[:,1], subtable[:,0], c=(rank_colors[k],12/k**2.5), marker=subrank_types[j][0], s=subrank_types[j][1])
+            if len(subtable) > 0:
+                plt.ylim((0,np.max(subtable[:,0])))
+                plt.savefig("Rank{0:0d}-{1:d}".format(k,j))
+                print("Rank{0:0d}-{1:d}".format(k,j))
+    k = 13
+    for j in range(6, 12):
         subtable = card_combination_table[card_combination_table[:,2] == 10*k + j]
-        ax.scatter(subtable[:,1], subtable[:,0], c=(rank_colors[k],1/k), marker=subrank_types[j])
-        plt.ylim((0,np.max(subtable[:,0])))
+        ax.scatter(subtable[:,1], subtable[:,0], c=(rank_colors[k],12/k**3), marker=subrank_types[2][0], s=subrank_types[2][1])
+        if len(subtable) > 0:
+            plt.ylim((np.min(subtable[:,0]),np.max(subtable[:,0])))
         plt.savefig("Rank{0:0d}-{1:d}".format(k,j))
+        print("Rank{0:0d}-{1:d}".format(k,j))
 
-plt.show()
-print(impossibles)
+    plt.show()
+
+def one_two_combinations():
+    fig, axs = plt.subplot_mosaic([['possible_combinations']], layout='constrained')
+    ax = axs['possible_combinations']
+    ax.set_yscale('linear')
+    plt.figure(figsize=(9,16))
+
+    subtable = card_combination_table[card_combination_table[:,2] < 40]
+    ax.scatter(subtable[:,1], subtable[:,3], c=(rank_colors[1],0.25), s = subrank_types[0][1])
+    plt.savefig("OneTwo{0:0d}-{1:d}".format(0,0))
+    
+    for k in range(4,14):
+        for j in range(0,3):
+            fig.clear()
+            fig, axs = plt.subplot_mosaic([['possible_combinations']], layout='constrained')
+            ax = axs['possible_combinations']
+            subtable = card_combination_table[card_combination_table[:,2] == 10*k + j]
+            ax.scatter(subtable[:,1], subtable[:,3], c=(rank_colors[k],12/k**2.5), marker=subrank_types[j][0], s=subrank_types[j][1])
+            if len(subtable) > 0:
+                plt.savefig("OneTwo{0:0d}-{1:d}".format(k,j))
+                print("OneTwo{0:0d}-{1:d}".format(k,j))
+    k = 13
+    for j in range(6, 12):
+        fig.clear()
+        fig, axs = plt.subplot_mosaic([['possible_combinations']], layout='constrained')
+        ax = axs['possible_combinations']
+        subtable = card_combination_table[card_combination_table[:,2] == 10*k + j]
+        ax.scatter(subtable[:,1], subtable[:,3], c=(rank_colors[k],12/k**3), marker=subrank_types[2][0], s=subrank_types[2][1])
+        plt.savefig("OneTwo{0:0d}-{1:d}".format(k,j))
+        print("OneTwo{0:0d}-{1:d}".format(k,j))
+
+    plt.show()
+
+def impossible_percentage_rank(BASE, SUBINTERVAL):
+
+    subtable = card_combination_table[card_combination_table[:,2] < 40]
+    fig, axs = plt.subplot_mosaic([['percentage_impossible']], layout='constrained', figsize=(64,36))
+    ax = axs['percentage_impossible']
+    ax.set_yscale('linear')
+
+    subtable = card_combination_table[card_combination_table[:,2] % 10 == 3]
+    ax.scatter(subtable[:,1] - 16, np.log(subtable[:,0])/np.log(BASE), c=('red',1/50), marker='x', s=5)
+
+    subtable = card_combination_table[card_combination_table[:,2] < 40]
+    ax.scatter(subtable[:,1], np.log(subtable[:,0])/np.log(BASE), c=(rank_colors[1],0.64), s = subrank_types[0][1])
+    plt.xlim((-5,500))
+    plt.savefig("Rank{0:0d}-{1:d}".format(0,0))
+    print("Rank{0:0d}-{1:d}".format(0,0))
+
+    for k in range(4,14):
+        for j in range(0,3):
+            subtable = card_combination_table[card_combination_table[:,2] == 10*k + j]
+            ax.scatter(subtable[:,1], np.log(subtable[:,0])/np.log(BASE), c=(rank_colors[k],12/k**2.5), marker=subrank_types[j][0], s=subrank_types[j][1])
+            if len(subtable) > 0:
+                plt.savefig("Rank{0:0d}-{1:d}".format(k,j))
+                print("Rank{0:0d}-{1:d}".format(k,j))
+    k = 13
+    for j in range(6, 12):
+        subtable = card_combination_table[card_combination_table[:,2] == 10*k + j]
+        ax.scatter(subtable[:,1], np.log(subtable[:,0])/np.log(BASE), c=(rank_colors[k],12/k**3), marker=subrank_types[2][0], s=subrank_types[2][1])
+        # plt.savefig("Rank{0:0d}-{1:d}".format(k,j))
+        print("Rank{0:0d}-{1:d}".format(k,j))
+
+    ranks = []
+    for k in range(9*SUBINTERVAL,int((12 + np.log(4)/np.log(BASE) + 5)*SUBINTERVAL)):
+        # print(10**((k - 1)/10), 10**(k/10), 10**((k + 1)/10))
+        subtable = card_combination_table[ card_combination_table[:,0] >= BASE**(k/SUBINTERVAL) ]
+        subtable = subtable[ subtable[:,0] < BASE**((k + 1)/SUBINTERVAL) ]
+        impossible = subtable[ subtable[:,2] % 10 == 3 ]
+        if len(subtable) > 0:
+            impossibility = len(impossible)/len(subtable)
+            print("{0:8d}\t[ {1:6.2f}% ] {3:d}/{2:d} impossible".format(int(np.ceil(BASE**(k/SUBINTERVAL))), impossibility*100, len(subtable), len(impossible)))
+            ranks.append([k/SUBINTERVAL, impossibility])
+        else:
+            print("{0:8d}\t is empty".format(int(np.ceil(BASE**(k/SUBINTERVAL)))))
+            
+    ranks = np.array(ranks)
+    ax.plot(500*ranks[:,1],ranks[:,0], 'r')
+    ax.yaxis.set_major_formatter(lambda y, pos : "{0:d}".format(int(3**y)))
+    ax.yaxis.set_minor_formatter(lambda y, pos : "{0:d}".format(int(3**y)))
+    ax.xaxis.set_minor_formatter(ticker.FormatStrFormatter("%d%%"))
+
+    secax = ax.secondary_xaxis('top', functions=(lambda x: x / 5, lambda x : x * 5))
+    ax.set_xlabel('number of combinations (exact)')
+    ax.set_ylabel('score')
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(1,0))
+    ax.yaxis.set_minor_locator(ticker.MultipleLocator(1,np.log(4)/np.log(3)))
+    ax.yaxis.set_minor_locator(ticker.MultipleLocator(1,np.log((3**13+1)/2)/np.log(3) - 12))
+    ax.set_title('Number of card combinations and impossible scores in Threes')
+    secax.set_xlabel('percentage impossible (in increment)')
+
+    plt.savefig("Impossible_Percentage", dpi=120)
+
+    for j in range(6, 12):
+        subtable = card_combination_table[card_combination_table[:,2] == 10*k + j]
+        if len(subtable) > 0:
+            plt.ylim((np.min(np.log(subtable[:,0])/np.log(BASE)),np.max(np.log(subtable[:,0])/np.log(BASE))))
+            plt.savefig("Impossible_Percentage 13-{0:d}".format(j), dpi=120)
+    
+    # plt.show()
+
+impossible_percentage_rank(3,16)
